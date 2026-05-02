32dd99ee20df77fbfba4bf4ac34b7fd0b0c5736e
mpandaer <liwenhaolx@gmail.com>
2026-05-02 22:20:33 +0800

    修改网站标题

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,7 +4,7 @@ import { generateSiteNavigation } from '../scripts/generate-nav.js'
 const { nav, sidebar } = generateSiteNavigation()
 
 export default defineConfig({
-  title: '我的博客',
+  title: 'Pandaer写东西的地方',
   description: '个人博客与作品集网站',
   base: '/',
   srcDir: 'content',
